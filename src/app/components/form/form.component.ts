cc446f67fa3da8aefedaf3b11dd2733a7cd8d39c
Arthur-Cunhax <arthurcl634@gmail.com>
2024-11-16 23:06:17 -0300

    16/11

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { ReturnStatement } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
 
+
+
+
+  listaTarefas : any[] = []
+  constructor() { }
+
+  ngOnInit(): void {
+    this.listaTarefas= [
+      {id: 0 , nome: "TarefaExemplo" ,dataEntrega : "10/02/2012" ,concluida : false}
+    ]
+  }
+  adicionar(tarefaNome:string,dataEntrega:string){
+    if (tarefaNome.trim().length == 0 || dataEntrega.trim().length == 0){
+      return;
+    }
+
+    const tarefaRepetida = this.listaTarefas.find(item => item.nome.toLowerCase() == tarefaNome.toLowerCase())
+    if (!tarefaRepetida){
+      this.listaTarefas.push({id: this.listaTarefas.length, nome : tarefaNome , dataEntrega : dataEntrega ,concluida : false})
+    }
+  }
+  deletar(tarefaid: number){
+    this.listaTarefas = this.listaTarefas.filter(tarefa => tarefa.id !== tarefaid);
+  }
 }
+
